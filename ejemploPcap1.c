41aeb555ee8773052822e06059a02662b18c9c08
encinas4 <javier.encinasc@estudiante.uam.es>
2018-10-15 16:32:09 +0000

    Segunda version, implementado con un arg, no implementado con dos args

--- ejemploPcap1.c
+++ ejemploPcap1.c
@@ -17,13 +17,50 @@ pcap_t *descr=NULL,*descr2=NULL;
 pcap_dumper_t *pdumper=NULL;
 int count_paquetes = 0;
 int num_bytes;
+char file_name[256];
 
+/*
+ * funcion para la impresion de los paquetes por pantalla
+*/
+
+/*
+void n_bytes(int n, char *f) {
+
+	FILE *fichero = NULL;
+	int i=0;
+	int c;
+	char *resultado = " ";
+	char * errbuf;
+
+	fichero = fopen(f,"r");
+	if(fichero==NULL) {
+		printf("Error al leer el fichero para los n Bytes\n");
+	}
+
+	if(n == 0) {
+		printf("No hay nada que imprimir si se quieren mostrar %d Bytes\n", n);
+	} else {
+
+		while( i < (2*n)) {
+			c = fgetc(fichero);
+			if(c != 32){
+				printf("%x ", c);
+				i++;
+			}
+		}
+	}
+
+	printf("%s\n", resultado);
+	fclose(fichero);
+}
+*/
 
 /*
 	funcion para la gestion del Ctrl-C
 */
 void handle(int nsignal){
 	printf("Control C pulsado\n");
+	
 	if(descr)
 		pcap_close(descr);
 	if(descr2)
@@ -32,8 +69,7 @@ void handle(int nsignal){
 		pcap_dump_close(pdumper);
 
 	printf("\nNumero de paquetes recibidos por eth0: %d\n", count_paquetes);
-	
-	n_bytes(num_bytes, file_name);
+	/*n_bytes(num_bytes, file_name);*/
 
 	exit(OK);
 }
@@ -44,54 +80,45 @@ void handle(int nsignal){
 */  
 void fa_nuevo_paquete(uint8_t *usuario, const struct pcap_pkthdr* cabecera, const uint8_t* paquete){
 	int* num_paquete=(int *)usuario;
-	struct pcap_pkthdr cabeceraTiempo;
+	struct pcap_pkthdr cabeceraAux;
+	const uint8_t *paqueteAux = paquete;
+	int maxBytes = 0, i;
+
 
 	/*Aumentamos en uno el paquete leido*/
 	(*num_paquete)++;
 	count_paquetes ++;
 
+	/*Imprimimos los N_Bytes del paquete*/
+	if(cabecera->len < num_bytes) {
+		maxBytes = cabecera->len;
+	} else {
+		maxBytes = num_bytes;
+	}
+
+	for(i=0; i < maxBytes; i++) {
+		printf("%02X", *paqueteAux);
+		paqueteAux = paqueteAux + sizeof(uint8_t);
+	}
+	printf("\n");
+
 	/*Creamos una nueva cabecera igual que la anterior pero con el tiempo modificado en 30 minutos adelantados*/
-	cabeceraTiempo.ts.tv_sec = cabecera->ts.tv_sec + 1800;
-	cabeceraTiempo.ts.tv_usec = cabecera->ts.tv_usec;
-	cabeceraTiempo.len = cabecera->len;
-	cabeceraTiempo.caplen = cabecera->caplen;
+	cabeceraAux.ts.tv_sec = cabecera->ts.tv_sec + 1800;
+	cabeceraAux.ts.tv_usec = cabecera->ts.tv_usec;
+	cabeceraAux.len = cabecera->len;
+	cabeceraAux.caplen = cabecera->caplen;
 
 	/*Capturamos el paquete*/
 	printf("Nuevo paquete capturado a las %s\n",ctime((const time_t*)&(cabecera->ts.tv_sec)));
 	if(pdumper){
-		pcap_dump((uint8_t *)pdumper,&cabeceraTiempo,paquete);
+		pcap_dump((uint8_t *)pdumper,&cabeceraAux,paquete);
 	}
 }
 
-void n_bytes(int n, char *f) {
-	FILE *fichero = fopen(f, "r");
-	int i=0;
-	int c;
-	char *resultado = " ";
 
-	if(!fichero) {
-		printf("Error al leer el fichero para los n Bytes\n");
-	}
-	if(n == 0) {
-		printf("No hay nada que imprimir si se quieren mostrar %d Bytes\n", n);
-	} else{
-		while(i<(2*n)) {
-			c = fgetc(fichero);
-			if(c != 32){
-				printf("%x ", c);
-				i++;
-			}
-		}
-	}
-	printf("%s\n", resultado);
-	fclose(fichero);
-}
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	int retorno=0, contador=0;
 	char errbuf[PCAP_ERRBUF_SIZE];
-	char file_name[256];
 	struct timeval time;
 
 	/* Caso 1: ningun argumento */
@@ -101,8 +128,14 @@ int main(int argc, char **argv)
 		fprintf (stdout, "\t\t\t./ejemploPcap1 \tnumero_bytes_a_mostrar\n"); 
 		fprintf (stdout, "\t\t\t./ejemploPcap1 \tnumero_bytes_a_mostrar \tnombre_traza.pcap\n"); 
 		return -1;
+	}
 
-	} else if(argc == 2) {		/* Caso 2: un argumento, el numero de Bytes a mostrar de los paquetes*/
+	if(argc == 2) {		/* Caso 2: un argumento, el numero de Bytes a mostrar de los paquetes*/
+
+		if( signal(SIGINT,handle)==SIG_ERR ){
+			printf("Error: Fallo al capturar la senal SIGINT.\n");
+			exit(ERROR);
+		}
 
 		gettimeofday(&time,NULL);	
 		sprintf(file_name,"eth0.%lld.pcap",(long long)time.tv_sec);		/*	file_name: nombre de la traza donde vamos a volcar los pqts */
@@ -113,12 +146,12 @@ int main(int argc, char **argv)
 		/* para poder almacenar en file_name */
 		descr2 = pcap_open_dead(DLT_EN10MB,1514);
 		if( descr2 == NULL ){
-			printf("Algo fue mal en pcap_open_dead \n");
+			printf("Error: pcap_open_dead() \n");
 			exit(ERROR);
 		}
 		pdumper = pcap_dump_open(descr2,file_name);
 		if( pdumper == NULL ){
-			printf("Algo fue mal en pcap_dump_open \n");
+			printf("Error: pcap_dump_open() \n");
 			pcap_close(descr2);
 			exit(ERROR);
 		}
@@ -146,24 +179,16 @@ int main(int argc, char **argv)
 		else if(retorno == 0){
 			printf("No mas paquetes o limite superado %s %d.\n",__FILE__,__LINE__);
 		}
-		
-		if(signal(SIGINT,handle)==SIG_ERR){
-			printf("Error: Fallo al capturar la senal SIGINT.\n");
-			exit(ERROR);
-		}
 
 	} else if(argc == 3){
-		
 		/* recuperamos el numero de bytes a imprimir por pantalla */
 		sscanf (argv[1],"%d",&num_bytes);
-		n_bytes(num_bytes, argv[2]);
+		pcap_open_offline(argv[2], errbuf);
+
 	} else {
 		fprintf(stdout, "El numero de argumentos no es valido, por favor, trate de introducir uno, dos o ninguno\n");
+		return -1;
 	}
-	
-	pcap_dump_close(pdumper);
-	pcap_close(descr);
-	pcap_close(descr2);
 
 	return OK;
 }
